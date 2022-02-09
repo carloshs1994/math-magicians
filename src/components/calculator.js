985fc345cecc66ceb5df5ab3607fc893045c8b9e
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-02-09 00:27:34 -0600

    Add logic to the App, the calculate function is not working yet

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,31 +1,47 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
+  FirstObject = {
+    next: null,
+    operation: null,
+  };
+
   constructor(props) {
     super(props);
     this.arrOfValues = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    this.state = { total: '0' };
   }
 
   PrintButtons = ({ arr }) => arr.map((value) => this.CalculatorButton(value));
 
+  PressButton = ({ target }) => {
+    const ResultObj = calculate({ total: `${target.innerText}`, ...this.FirstObject }, `${target.innerText}`);
+    this.FirstObject.next = ResultObj.next;
+    this.FirstObject.operation = ResultObj.operation;
+    console.log(target.innerText, this.FirstObject);
+    console.log(ResultObj);
+  };
+
   CalculatorButton = (value) => {
     if (value === '0') {
-      return (<button className="calculator-button-0" key={value} type="button">{value}</button>);
+      return (<button onClick={this.PressButton} className="calculator-button-0" key={value} type="button">{value}</button>);
     } if (value === '÷'
     || value === 'x'
     || value === '-'
     || value === '+'
     || value === '=') {
-      return (<button className="calculator-button-operation" key={value} type="button">{value}</button>);
+      return (<button onClick={this.PressButton} className="calculator-button-operation" key={value} type="button">{value}</button>);
     }
-    return (<button key={value} type="button">{value}</button>);
+    return (<button onClick={this.PressButton} key={value} type="button">{value}</button>);
   };
 
   render() {
+    const { total } = this.state;
     return (
       <section className="calculator">
         <div className="calculator-screen">
-          <p>0</p>
+          <p>{total}</p>
         </div>
         <this.PrintButtons arr={this.arrOfValues} />
       </section>
